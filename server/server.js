9b70ace1fba8c0181639e464f089dc6d9211e886
Brandon <bpl5@students.uwf.edu>
2018-06-03 01:47:02 -0400

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -22,7 +22,20 @@ app.post('/todos', (req, res) => {
     res.send(doc);
   }, (e) => {
     res.status(400).send(e);
-  })
+  });
+});
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
 });
 
 app.get('/todos', (req, res) =>{
@@ -86,6 +99,7 @@ app.patch('/todos/:id', (req, res) =>{
     res.status(400).send();
   });
 });
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
